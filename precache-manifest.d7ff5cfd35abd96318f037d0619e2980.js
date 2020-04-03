438e531e65c3469fa1d92fcca9b3a5bd8bcd77ad
puch <puchinaps@gmail.com>
2020-04-03 19:41:11 +0200

    Updates

---
+++ precache-manifest.d7ff5cfd35abd96318f037d0619e2980.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d614decfacdb03623f819fbdc3f677a",
+    "url": "/marvelStudio/index.html"
+  },
+  {
+    "revision": "e98d0d53989f2ce4dc9b",
+    "url": "/marvelStudio/static/css/3.9c82ed06.chunk.css"
+  },
+  {
+    "revision": "8ed0799dbcb3e78ccb92",
+    "url": "/marvelStudio/static/css/4.7fe07bcb.chunk.css"
+  },
+  {
+    "revision": "3262a4bd63c80ec46185",
+    "url": "/marvelStudio/static/css/5.e8d4b6c3.chunk.css"
+  },
+  {
+    "revision": "f65e0722fa7d250b3d06",
+    "url": "/marvelStudio/static/css/main.0adf8a30.chunk.css"
+  },
+  {
+    "revision": "ee82732bb2b7b3e08dcc",
+    "url": "/marvelStudio/static/js/2.c60d399b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/marvelStudio/static/js/2.c60d399b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e98d0d53989f2ce4dc9b",
+    "url": "/marvelStudio/static/js/3.7f1094a3.chunk.js"
+  },
+  {
+    "revision": "8ed0799dbcb3e78ccb92",
+    "url": "/marvelStudio/static/js/4.2e0e0e50.chunk.js"
+  },
+  {
+    "revision": "3262a4bd63c80ec46185",
+    "url": "/marvelStudio/static/js/5.63487dc6.chunk.js"
+  },
+  {
+    "revision": "f65e0722fa7d250b3d06",
+    "url": "/marvelStudio/static/js/main.55bbec88.chunk.js"
+  },
+  {
+    "revision": "c3ad6d2214bf98ca6e2b",
+    "url": "/marvelStudio/static/js/runtime-main.5376fac0.js"
+  }
+]);
